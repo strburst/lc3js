737014a7e86cd1dc47d9f75172fbd3891182da9a
Allen Zheng <strburst@users.noreply.github.com>
2015-12-15 14:40:35 -0500

    Add basic warning code

---
+++ util.js
@@ -0,0 +1,27 @@
+var warningTable = {
+  'exec data': {
+    longName: 'execution of data',
+    explanation: 'A memory cell previously marked as an instruction has ' +
+      'been executed.'
+  },
+
+  'self-modifying': {
+    longName: 'self-modifying code',
+    explanation: 'A memory cell previously marked as an instruction has ' +
+      'been modified by the program.'
+  },
+
+  'read uninitd': {
+    longName: 'reading uninitialized memory',
+    explanation: 'A memory cell whose value was not previously set by the ' +
+      'program or implicitly defined as a service routine has been read'
+  }
+};
+
+exports.warn = function(warningName) {
+  warning = warningTable[warningName];
+
+  console.log('Warning: ' + warning.longName);
+  console.log();
+  console.log(warning.explanation);
+};
