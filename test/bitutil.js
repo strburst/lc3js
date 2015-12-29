ba20c78aa8e75bc3eb0628e83a9853991d83d657
Allen Zheng <strburst@users.noreply.github.com>
2015-12-29 11:04:07 -0500

    Clean up/flesh out bitutil tests significantly

--- test/bitutil.js
+++ test/bitutil.js
@@ -2,16 +2,26 @@ var assert = require('assert');
 
 var bitutil = require('../bitutil.js');
 
-describe('bitwise utilities', function() {
+describe('bit utilities', function() {
   describe('truncate', function() {
     it('shouldn\'t change normal numbers', function() {
-      assert.equal(bitutil.truncate(12), 12);
-      assert.equal(bitutil.truncate(-12), -12);
+      assert.equal(bitutil.truncate(parseInt('0110', 2), 16), 6, 'case where number wasn\'t truncated');
+      assert.equal(bitutil.truncate(parseInt('1010', 2), 16), 10, 'case where number wasn\'t truncated');
     });
 
-    it('should truncate big numbers properly', function() {
-      assert.equal(bitutil.truncate(16, 4), 0);
-      assert.equal(bitutil.truncate(15, 4), -1);
+    it('should sign extend based on the leading bit', function() {
+      assert.equal(bitutil.truncate(parseInt('0110', 2), 4), 6, 'case where leading bit was 0');
+      assert.equal(bitutil.truncate(parseInt('1010', 2), 4), -6, 'case where leading bit was 1');
+    });
+
+    it('should ignore higher order bits', function() {
+      assert.equal(bitutil.truncate(parseInt('10100110', 2), 4), 6, 'case where true leading bit was 0');
+      assert.equal(bitutil.truncate(parseInt('10101010', 2), 4), -6, 'case where true leading bit was 1');
+    });
+
+    it('should handle edge cases', function() {
+      assert.equal(bitutil.truncate(parseInt('10000', 2), 4), 0, 'given range has only zeroes');
+      assert.equal(bitutil.truncate(parseInt('01111', 2), 4), -1, 'given range has only ones');
     });
   });
 });
