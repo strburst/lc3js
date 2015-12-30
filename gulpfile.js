da43f16c85891e2eb8f8b1bf3b1f2313ad279d47
Allen Zheng <strburst@users.noreply.github.com>
2015-12-30 14:22:05 -0500

    Catch and log gulp errors nicely to avoid crashing

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var peg = require('gulp-peg');
 var rename = require('gulp-rename');
 
-gulp.task("compile-parser", function() {
+gulp.task('compile-parser', function() {
   return gulp.src('gen-parser.peg')
-    .pipe(peg())
+    .pipe(peg().on('error', gutil.log))
     .pipe(rename('parser.js'))
     .pipe(gulp.dest('.'));
 });
