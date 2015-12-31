9cf77572c110c0337d93e49e02615325a6817246
Allen Zheng <strburst@users.noreply.github.com>
2015-12-30 19:17:14 -0500

    Add tests for fetchBits and testBit

--- test/bitutil.js
+++ test/bitutil.js
@@ -24,5 +24,41 @@ describe('bit utilities', function() {
       assert.equal(bitutil.truncate(parseInt('01111', 2), 4), -1, 'given range has only ones');
     });
   });
+
+  describe('fetchBits', function() {
+    it('should fetch single bits', function() {
+      assert.equal(bitutil.fetchBits(parseInt('110100', 2), 2, 2), 0x1);
+      assert.equal(bitutil.fetchBits(parseInt('110100', 2), 1, 1), 0x0);
+    });
+
+    it('should fetch typical ranges', function() {
+      assert.equal(bitutil.fetchBits(parseInt('110100', 2), 2, 4), parseInt('101', 2));
+      assert.equal(bitutil.fetchBits(parseInt('110100', 2), 2, 5), parseInt('1101', 2));
+      assert.equal(bitutil.fetchBits(parseInt('110100', 2), 0, 2), parseInt('100', 2));
+    });
+
+    it('should fetch ranges in edge cases', function() {
+      assert.equal(bitutil.fetchBits(0x0, 3, 10), 0, 'number was zero');
+      assert.equal(bitutil.fetchBits(~0x0, 3, 10), 0xFF, 'number was all ones');
+    });
+  });
+
+  describe('testBit', function() {
+    it('should return single bits', function() {
+      assert.equal(bitutil.testBit(parseInt('110100', 2), 0), 0x0);
+      assert.equal(bitutil.testBit(parseInt('110100', 2), 1), 0x0);
+      assert.equal(bitutil.testBit(parseInt('110100', 2), 2), 0x1);
+      assert.equal(bitutil.testBit(parseInt('110100', 2), 3), 0x0);
+      assert.equal(bitutil.testBit(parseInt('110100', 2), 4), 0x1);
+      assert.equal(bitutil.testBit(parseInt('110100', 2), 5), 0x1);
+    });
+
+    it('should return the correct bit in edge cases', function() {
+      assert.equal(bitutil.testBit(0x0, 0), 0x0, 'number and index both zero');
+      assert.equal(bitutil.testBit(0x1, 0), 0x1, 'number is one and index is zero');
+
+      assert.equal(bitutil.testBit(0x2, 5), 0x0, 'index is large than number');
+    });
+  });
 });
 
