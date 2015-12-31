27ffea7a5ebaf3594ee5cdd3307973b3a7e4b22b
Allen Zheng <strburst@users.noreply.github.com>
2015-12-30 19:05:20 -0500

    Modify assert messages slightly
    Get rid of the 'case where' grammatical boilerplate.

--- test/bitutil.js
+++ test/bitutil.js
@@ -5,22 +5,18 @@ var bitutil = require('../bitutil.js');
 describe('bit utilities', function() {
   describe('truncate', function() {
     it('shouldn\'t change normal numbers', function() {
-      assert.equal(bitutil.truncate(parseInt('0110', 2), 16), 6, 'case where number wasn\'t ' +
-          'truncated');
-      assert.equal(bitutil.truncate(parseInt('1010', 2), 16), 10, 'case where number wasn\'t ' +
-          'truncated');
+      assert.equal(bitutil.truncate(parseInt('0110', 2), 16), 6, 'number wasn\'t truncated');
+      assert.equal(bitutil.truncate(parseInt('1010', 2), 16), 10, 'number wasn\'t truncated');
     });
 
     it('should sign extend based on the leading bit', function() {
-      assert.equal(bitutil.truncate(parseInt('0110', 2), 4), 6, 'case where leading bit was 0');
-      assert.equal(bitutil.truncate(parseInt('1010', 2), 4), -6, 'case where leading bit was 1');
+      assert.equal(bitutil.truncate(parseInt('0110', 2), 4), 6, 'leading bit was 0');
+      assert.equal(bitutil.truncate(parseInt('1010', 2), 4), -6, 'leading bit was 1');
     });
 
     it('should ignore higher order bits', function() {
-      assert.equal(bitutil.truncate(parseInt('10100110', 2), 4), 6, 'case where new leading bit ' +
-          'was 0');
-      assert.equal(bitutil.truncate(parseInt('10101010', 2), 4), -6, 'case where new leading bit ' +
-          'was 1');
+      assert.equal(bitutil.truncate(parseInt('10100110', 2), 4), 6, 'new leading bit was 0');
+      assert.equal(bitutil.truncate(parseInt('10101010', 2), 4), -6, 'new leading bit was 1');
     });
 
     it('should handle edge cases', function() {
