133178518554b3b46f45d9280487d4ca635c77fa
Allen Zheng <strburst@users.noreply.github.com>
2016-01-09 12:28:45 -0500

    Move the assembly test cases into a separate file

--- test/assembler.js
+++ test/assembler.js
@@ -1,124 +1,11 @@
 var assert = require('assert');
 
 var assembler = require('../assembler.js');
-var bitutil = require('../bitutil.js');
 var misc = require('../misc.js');
 
-describe('bit utilities', function() {
-  // Test cases for assembly and dissasembly
-  var assemblerCases = [
-    {
-      binary: bitutil.fromBits('0001', '110', '001', '0', '00', '010'),
-      message: 'typical ADD (register form)',
-      object: {
-        operation: 'ADD',
-        destReg: 6,
-        srcReg1: 1,
-        srcReg2: 2,
-      },
-    },
-    {
-      binary: bitutil.fromBits('0001', '101', '001', '1', '10101'),
-      message: 'typical ADD (immediate form)',
-      object: {
-        operation: 'ADD',
-        destReg: 5,
-        srcReg1: 1,
-        immediate: 21,
-      },
-    },
-    {
-      binary: bitutil.fromBits('0101', '110', '001', '0', '00', '010'),
-      message: 'typical AND (register form)',
-      object: {
-        operation: 'AND',
-        destReg: 6,
-        srcReg1: 1,
-        srcReg2: 2,
-      },
-    },
-    {
-      binary: bitutil.fromBits('0101', '101', '001', '1', '10101'),
-      message: 'typical AND (immediate form)',
-      object: {
-        operation: 'AND',
-        destReg: 5,
-        srcReg1: 1,
-        immediate: 21,
-      },
-    },
-    {
-      binary: 0xAA,
-      message: 'typical BR',
-      object: {
-        operation: 'BR',
-        conditionCode: { n: false, z: false, p: false },
-        offset: 0xAA,
-      },
-    },
-    {
-      binary: bitutil.fromBits('1100', '000', '101', '000000'),
-      message: 'typical JMP',
-      object: {
-        operation: 'JMP',
-        register: 5,
-      },
-    },
-    {
-      binary: bitutil.fromBits('0100', '1', '00010101010'),
-      message: 'typical JSR',
-      object: {
-        operation: 'JSR',
-        offset: 0xAA,
-      },
-    },
-    {
-      binary: bitutil.fromBits('0100', '0', '00', '101', '000000'),
-      message: 'typical JSRR',
-      object: {
-        operation: 'JSR',
-        register: 5,
-      },
-    },
-    {
-      binary: bitutil.fromBits('0010', '101', '010101010'),
-      message: 'typical LD',
-      object: {
-        operation: 'LD',
-        offset: 0xAA,
-        register: 5,
-      },
-    },
-    {
-      binary: bitutil.fromBits('1010', '101', '010101010'),
-      message: 'typical LDI',
-      object: {
-        operation: 'LDI',
-        offset: 0xAA,
-        register: 5,
-      },
-    },
-    {
-      binary: bitutil.fromBits('0110', '101', '010', '101010'),
-      message: 'typical LDR',
-      object: {
-        operation: 'LDR',
-        moveReg: 5,
-        baseReg: 2,
-        offset: 0x2A,
-      },
-    },
-    {
-      binary: bitutil.fromBits('1110', '101', '010101010'),
-      message: 'typical LEA',
-      object: {
-        operation: 'LEA',
-        offset: 0xAA,
-        register: 5,
-      },
-    },
-  ];
+var assemblerCases = require('./assemblerCases.js');
 
+describe('bit utilities', function() {
   // Sort individual test cases by operation tested
   assemblerCases = misc.distinctMap(function(item) {
     return item.object.operation;
@@ -138,6 +25,7 @@ describe('bit utilities', function() {
 
   describe('toBits', function() {
     // Pass these parameters in every test (thus all offsets will be 0xAA)
+    // TODO: Add custom offsets, derived from the offset field in operation.object
     var nextAddr = 0x4000;
     var labelToAddr = { ARGLABEL: 0x40AA };
 
---
+++ test/assemblerCases.js
@@ -0,0 +1,118 @@
+var bitutil = require('../bitutil.js');
+
+// Test cases for assembly and dissasembly
+module.exports = [
+  {
+    // Correct assembled representation
+    binary: bitutil.fromBits('0001', '110', '001', '0', '00', '010'),
+    // Message passed to assert.equal
+    message: 'typical ADD (register form)',
+    // Correct disassembled representation
+    object: {
+      operation: 'ADD',
+      destReg: 6,
+      srcReg1: 1,
+      srcReg2: 2,
+    },
+  },
+  {
+    binary: bitutil.fromBits('0001', '101', '001', '1', '10101'),
+    message: 'typical ADD (immediate form)',
+    object: {
+      operation: 'ADD',
+      destReg: 5,
+      srcReg1: 1,
+      immediate: 21,
+    },
+  },
+  {
+    binary: bitutil.fromBits('0101', '110', '001', '0', '00', '010'),
+    message: 'typical AND (register form)',
+    object: {
+      operation: 'AND',
+      destReg: 6,
+      srcReg1: 1,
+      srcReg2: 2,
+    },
+  },
+  {
+    binary: bitutil.fromBits('0101', '101', '001', '1', '10101'),
+    message: 'typical AND (immediate form)',
+    object: {
+      operation: 'AND',
+      destReg: 5,
+      srcReg1: 1,
+      immediate: 21,
+    },
+  },
+  {
+    binary: 0xAA,
+    message: 'typical BR',
+    object: {
+      operation: 'BR',
+      conditionCode: { n: false, z: false, p: false },
+      offset: 0xAA,
+    },
+  },
+  {
+    binary: bitutil.fromBits('1100', '000', '101', '000000'),
+    message: 'typical JMP',
+    object: {
+      operation: 'JMP',
+      register: 5,
+    },
+  },
+  {
+    binary: bitutil.fromBits('0100', '1', '00010101010'),
+    message: 'typical JSR',
+    object: {
+      operation: 'JSR',
+      offset: 0xAA,
+    },
+  },
+  {
+    binary: bitutil.fromBits('0100', '0', '00', '101', '000000'),
+    message: 'typical JSRR',
+    object: {
+      operation: 'JSR',
+      register: 5,
+    },
+  },
+  {
+    binary: bitutil.fromBits('0010', '101', '010101010'),
+    message: 'typical LD',
+    object: {
+      operation: 'LD',
+      offset: 0xAA,
+      register: 5,
+    },
+  },
+  {
+    binary: bitutil.fromBits('1010', '101', '010101010'),
+    message: 'typical LDI',
+    object: {
+      operation: 'LDI',
+      offset: 0xAA,
+      register: 5,
+    },
+  },
+  {
+    binary: bitutil.fromBits('0110', '101', '010', '101010'),
+    message: 'typical LDR',
+    object: {
+      operation: 'LDR',
+      moveReg: 5,
+      baseReg: 2,
+      offset: 0x2A,
+    },
+  },
+  {
+    binary: bitutil.fromBits('1110', '101', '010101010'),
+    message: 'typical LEA',
+    object: {
+      operation: 'LEA',
+      offset: 0xAA,
+      register: 5,
+    },
+  },
+];
